064a6f64b7ac1c8a4595cd32e6eb751547d91ca5
Daniel Castro <contatodanielcastro@gmail.com>
2023-01-17 16:13:32 -0300

    Script

---
+++ checkssl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+data=`echo | openssl s_client -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -enddate | sed -e 's#notAfter=##'`
+
+ssldate=`date -d "${data}" '+%s'`
+nowdate=`date '+%s'`
+diff="$((${ssldate}-${nowdate}))"
+
+echo $((${diff}/86400))
